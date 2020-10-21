bed6273818eff7aed95f424035da2e57f5a9438e
skeev_ <derkach94@gmail.com>
2020-10-21 15:16:47 +0300

    404

---
+++ src/views/pages/404/404.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// js, scss of components
+import screenCallback from '../../components/screen/screenCallback';
+import button from '../../components/button/button';
+
+// page styles
+import './404.scss';
+
+// initialise components and modules
+screenCallback.init();

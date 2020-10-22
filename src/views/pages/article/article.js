3a01985336d0db04a5d37f95546b621aecad92ba
skeev_ <derkach94@gmail.com>
2020-10-22 15:33:30 +0300

    article page

---
+++ src/views/pages/article/article.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+// js, scss of components
+import fluid from '../../components/fluid/fluid';
+import breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
+import button from '../../components/button/button';
+import screenAlso from '../../components/screen/screenAlso';
+
+// page styles
+import './article.scss';
+
+// init
